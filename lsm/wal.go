df5aa37e3fa25db19b802ee03ef6d40b80c0701c
Justin Ethier <justin.ethier@gmail.com>
2021-11-11 13:30:22 -0800

    Added ideas for WAL

--- lsm/wal.go
+++ lsm/wal.go
@@ -7,3 +7,8 @@ package lsm
 // WAL is a write-ahead log that is used to provide the durability of data during system failures, what it means is that when a request for write comes in, the data is first added to a WAL file (sometimes called journal) and flushed to the disk (using direct io) before updating the in-memory data structure.
 // This allows for systems to recover from the WAL if it crashes before persisting the in-memory data structure to disk.
 // Why not directly update the write to the disk instead of updating WAL? it’s because WAL updates are cheaper as it’s append-only and doesn’t require any restructuring of data on disk.
+
+
+// Other considerations:
+// - general background: https://martinfowler.com/articles/patterns-of-distributed-systems/wal.html
+// - fsync (maybe use initially?) - https://stackoverflow.com/a/10862573/101258
