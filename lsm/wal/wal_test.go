0a6a0f09b4e348feb72303f1d2519584a9de7f75
Justin Ethier <justin.ethier@gmail.com>
2021-11-15 09:22:26 -0800

    Added TODO's

--- lsm/wal/wal_test.go
+++ lsm/wal/wal_test.go
@@ -5,8 +5,20 @@ import (
 )
 
 func TestBasic (t *testing.T) {
+  // TODO: delete all files from disk
   wal := New(".")
   wal.Append("a", []byte("a string"))
   wal.Append("b", []byte("a string"))
   wal.Append("c", []byte("a string"))
 }
+
+
+// TODO: test failover by running one test to build up a WAL then
+// spin up another fresh wal instance and populate it with data
+// save by the first one
+
+// func TestRecovery(t *testing.T) {
+// }
+
+
+// TODO: test recovery again from a snapshot. EG: recover up to ID X
