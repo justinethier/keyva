0fe2bdfdb3dcdbe3e1c0381f7a97e558cc0bc24a
Justin Ethier <justin.ethier@gmail.com>
2021-11-28 14:19:29 -0800

    Initial file

---
+++ cmd/keyva-load/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func get(key string) {
+	resp, err := http.Get("http://localhost:8080/kv/" + key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	//Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	log.Printf(sb)
+}
+
+func set(key string, contentType string, data []byte) {
+	responseBody := bytes.NewBuffer(data)
+	//Leverage Go's HTTP Post function to make request
+	resp, err := http.Post("http://localhost:8080/kv/"+key, "application/json", responseBody)
+	//Handle Error
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+	//Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	log.Printf(sb)
+}
+
+func main() {
+	set("data-test", "text/plain", []byte("testing 1, 2, 3..."))
+	get("data-test")
+}
