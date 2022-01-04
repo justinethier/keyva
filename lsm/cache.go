6d5e161d0de65d8fb2274d32f1a46e7339e834b1
Justin Ethier <justin.ethier@gmail.com>
2022-01-03 18:57:16 -0800

    Refactoring

---
+++ lsm/cache.go
@@ -0,0 +1,28 @@
+package lsm
+
+import (
+	"github.com/justinethier/keyva/lsm/sst"
+	"fmt"
+	"time"
+)
+
+// CacheGcTimeout is the amount of time in seconds to keep a cached SST file in
+// memory before it is eligble for collection.
+var CacheGcTimeout = 300.0
+
+// CacheGC removes old entries from the cache
+func (tree *LsmTree) CacheGC() {
+	// TODO: would it be more efficient if we lock at the segment (sstfile) level?
+	tree.lock.Lock()
+	defer tree.lock.Unlock()
+
+	var empty []sst.SstEntry
+	for i := 0; i < len(tree.files); i++ {
+		fmt.Println("File", tree.files[i].Filename, "cached", len(tree.files[i].cache))
+		if len(tree.files[i].Cache) > 0 &&
+			time.Since(tree.files[i].CachedAt).Seconds() > CacheGcTimeout {
+			fmt.Println("Clear cache for file", tree.files[i].filename)
+			tree.files[i].Cache = empty
+		}
+	}
+}
