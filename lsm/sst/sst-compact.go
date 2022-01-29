477076559105bb038b5ca30f04d95884f5dfb7c4
Justin Ethier <justin.ethier@gmail.com>
2022-01-28 18:36:37 -0800

    Added notes

--- lsm/sst/sst-compact.go
+++ lsm/sst/sst-compact.go
@@ -23,20 +23,32 @@ import (
 // loads all file contents into memory. Then we have both and can benchmark / compare
 // them against different data sets.
 
+
+// TODO: modify Compact function below to -
+// - accept directory of lower-level-path (EG: L0), directory of higher-level path (previous L + 1)
+// - higher-level dir may not exist
+// - ... but if it does exist, need to merge those files in too
+// - generate multiple new files of size n (specified by caller?? config somewhere??)
+// - return names of old files to caller so they can be deleted
+
+// At a high level we want to do the following:
+// - create new compacted files. This can be done without holding any locks because all of the SST files are immutable
+// - write new files into directories off to the side somewhere, so they will not be accessed
+// - caller locks LSM, swaps (moves?) new files in, swaps old files out, and deletes them
+// - consider using manifest file or such to indicate which directories/files are used by the LSM for its SSTs
+// - when locked and caller does swaps, it also needs to clear any old files from memory, including cached contents
+
+
+// deleting old files
+//   - do with the appropriate locks
+//   - replace all files in l+1 ?
+//   - delete all files from l that were compacted with higher level. may still be files remaining in l if a flush was performed while compaction was running
+//   - delete (or some portions) may need to be done by LSM because it caches SST file contents
+
+
+
 // Compact implements a simple algorithm to load all SST files at the given path into memory, compact their contents, and write the contents back out to filename.
 func Compact(path string, filename string) {
-
-	// TODO: modify to -
-	// - accept directory of lower-level-path (EG: L0), directory of higher-level path (previous L + 1)
-	// - higher-level dir may not exist
-	// - ... but if it does exist, need to merge those files in too
-	// - generate multiple new files of size n
-	// - deleting old files
-	//   - do with the appropriate locks
-	//   - replace all files in l+1 ?
-	//   - delete all files from l that were compacted with higher level. may still be files remaining in l if a flush was performed while compaction was running
-	//   - delete (or some portions) may need to be done by LSM because it caches SST file contents
-
 	filenames := Filenames(path)
 	fmt.Println(filenames)
 
