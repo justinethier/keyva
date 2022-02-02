022f1ee3bffdecb40a2dfa2c200d1a8d22a0478b
Justin Ethier <justin.ethier@gmail.com>
2022-02-01 20:02:56 -0800

    Fix unit test

--- lsm/sst/compact_test.go
+++ lsm/sst/compact_test.go
@@ -6,8 +6,13 @@ import (
 )
 
 func TestSstCompact(t *testing.T) {
-	Compact("test-data", "newsst")
-	if !util.DeepCompare("newsst", "test-data/compacted.sst") {
+  var files []string
+
+  files = append(files, "./test-data/sorted-string-table-0000.json")
+  files = append(files, "./test-data/sorted-string-table-0001.json")
+  files = append(files, "./test-data/sorted-string-table-0002.json")
+	newdir, _ := Compact(files, "test-data", 100)
+	if !util.DeepCompare(newdir + "/sorted-string-table-0000.json", "test-data/compacted.sst") {
 		t.Error("Compacted SST file does not contain expected contents", "newsst")
 	}
 }
