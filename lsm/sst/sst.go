c0ad465ca1643fc143ff6b5e7c66034f3d80a04d
Justin Ethier <justin.ethier@gmail.com>
2022-02-03 10:10:09 -0800

    Extract IO into new file, add streaming TODO's

--- lsm/sst/sst.go
+++ lsm/sst/sst.go
@@ -1,44 +1,13 @@
 package sst
 
 import (
-	"bufio"
-	"encoding/json"
 	"fmt"
-	"github.com/justinethier/keyva/util"
 	"io/ioutil"
 	"log"
-	"os"
 	"regexp"
 	"strconv"
 	"time"
 )
-
-// Create creates a new SST file from given data
-func Create(filename string, keys []string, m map[string]SstEntry, seqNum uint64) {
-	f, err := os.Create(filename)
-	check(err)
-
-	defer f.Close()
-
-	header := SstFileHeader{seqNum}
-	b, err := json.Marshal(header)
-	check(err)
-	_, err = f.Write(b)
-	check(err)
-	_, err = f.Write([]byte("\n"))
-	check(err)
-
-	for _, k := range keys {
-		b, err := json.Marshal(m[k])
-		check(err)
-
-		_, err = f.Write(b)
-		check(err)
-
-		_, err = f.Write([]byte("\n"))
-		check(err)
-	}
-}
 
 func check(e error) {
 	if e != nil {
@@ -113,39 +82,6 @@ func NextFilename(path string) string {
 	}
 
 	return "sorted-string-table-0000.json"
-}
-
-//
-func Load(filename string, path string) ([]SstEntry, SstFileHeader) {
-	var buf []SstEntry
-	var header SstFileHeader
-
-	f, err := os.Open(path + "/" + filename)
-	if err != nil {
-		log.Println("Load error", err)
-		return buf, header
-	}
-
-	defer f.Close()
-
-	r := bufio.NewReader(f)
-	str, e := util.Readln(r)
-	check(e)
-	err = json.Unmarshal([]byte(str), &header)
-	check(e)
-	//fmt.Println("DEBUG SST header", header)
-
-	str, e = util.Readln(r)
-	for e == nil {
-		var data SstEntry
-		err = json.Unmarshal([]byte(str), &data)
-		check(err)
-		//fmt.Println(data)
-		buf = append(buf, data)
-		str, e = util.Readln(r)
-	}
-
-	return buf, header
 }
 
 // find searches the given entries for key and returns the corresponding value if found.
