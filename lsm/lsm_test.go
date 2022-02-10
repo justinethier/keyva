ef0add6737af566828d40ebf984a18cab2f6ad90
Justin Ethier <justin.ethier@gmail.com>
2022-02-10 06:33:24 -0800

    Add TODO items for more detailed unit testing of Merge()

--- lsm/lsm_test.go
+++ lsm/lsm_test.go
@@ -199,3 +199,74 @@ func TestSstKeyValueWithMerge(t *testing.T) {
 
 	tbl.ResetDB()
 }
+
+func TestSstMerge(t *testing.T) {
+	var N = 100
+	var tbl = New("testdb", 25)
+
+	tbl.ResetDB()
+
+	for i := N - 1; i >= 0; i-- {
+		// encode predictable value for i
+		tbl.Set(strconv.Itoa(i), []byte(strconv.Itoa(i)))
+	}
+
+	tbl.Delete(strconv.Itoa(100))
+	//tbl.Flush()
+
+	levels := sst.Levels("testdb")
+	if len(levels) != 0 {
+		t.Error("Found SST levels prior to merge", levels)
+	}
+
+TODO: specify a merge strategy here - X levels
+
+TODO: loop this all the way up to the max levels allowed
+verify expected number of sst files at each level
+
+TODO: verify if merge/compact fails on a level that is too high?
+should we have an explicit return value for that?
+	// Explicitly merge L0 to L1
+	tbl.Merge(0)
+
+	levels = sst.Levels("testdb")
+	if len(levels) != 1 {
+		t.Error("Did not find SST levels after merge", levels)
+	}
+
+	// verify i contains expected value
+	for i := 0; i < N; i++ {
+		if v, found := tbl.Get(strconv.Itoa(i)); found {
+			if bytes.Compare(v, []byte(strconv.Itoa(i))) != 0 {
+				t.Error("Unexpected value", v, "for key", i)
+			}
+		} else {
+			t.Error("Value not found for key", i)
+		}
+	}
+
+	for i := 0; i < N; i++ {
+		tbl.Delete(strconv.Itoa(i))
+	}
+
+	// verify key does not exist for i
+	for i := 0; i < N; i++ {
+		if val, found := tbl.Get(strconv.Itoa(i)); found {
+			t.Error("Unexpected value", val, "for deleted key", i)
+		}
+	}
+
+	// add a key back
+	tbl.Set("abcd", []byte("test"))
+
+	// verify that key exists now
+	if val, found := tbl.Get("abcd"); found {
+		if string(val) != "test" {
+			t.Error("Unexpected value", val, "for key", "abcd")
+		}
+	} else {
+		t.Error("Value not found for key", "abcd")
+	}
+
+	tbl.ResetDB()
+}
