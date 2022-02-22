21fedd4ece47c35b956acbe2cec9452b955c8b4e
Justin Ethier <justin.ethier@gmail.com>
2022-02-21 20:03:28 -0800

    Format, add more TODO's

--- cmd/datagen/main.go
+++ cmd/datagen/main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	//"bytes"
-  "fmt"
+	"fmt"
 	//"io/ioutil"
 	//"log"
 	//"net/http"
-  "math/rand"
-  "time"
 	"github.com/justinethier/keyva/lsm"
+	"math/rand"
+	"time"
 )
 
 TODO: modify to generate 1 MB, then 100 MB+ of data for the DB
+TODO: increase buffer size below
+TODO: configure merge settings
 
 func main() {
 	tbl := lsm.New("data", 5)
-  //tbl.ResetDB()
+	//tbl.ResetDB()
 
-  for i := 0; i < 125; i++ {
-    key := fmt.Sprintf("%d", rand.Intn(100))
-    doc := fmt.Sprintf("%d", time.Now().UnixNano())
-	  //set(key, "text/plain", []byte(doc))
-    tbl.Set(key, []byte(doc))
-  }
+	for i := 0; i < 125; i++ {
+		key := fmt.Sprintf("%d", rand.Intn(100))
+		doc := fmt.Sprintf("%d", time.Now().UnixNano())
+		//set(key, "text/plain", []byte(doc))
+		tbl.Set(key, []byte(doc))
+	}
 }
