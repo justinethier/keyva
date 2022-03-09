541b24afb466d2d093ee83e8e946763e1ca6311c
Justin Ethier <justin.ethier@gmail.com>
2022-03-08 19:43:34 -0800

    Finish functions to write binary data

--- lsm/sst/binary-io.go
+++ lsm/sst/binary-io.go
@@ -7,14 +7,62 @@ import (
 	"os"
 )
 
-func binaryWrite(f *os.File, data SstEntry) {
-  var bytes uint32 = uint32(utf8.RuneCountInString(data.Key))
-  //f.Write()
+func writeEntries(f *os.File, entries []SstEntry) (int, error){
+  var offset int = 0
+  for _, e := range entries {
+    bytes, err := writeEntry(f, e)
+    if err != nil {
+      return offset, err
+    }
+    offset += bytes
+    // TODO: keep track of every nth key/offset for the sparse index
+  }
+
+  return offset, nil
+}
+
+func writeEntry(f *os.File, data SstEntry) (int, error){
+  var bcount int = 0
+  var bytes int32 = int32(utf8.RuneCountInString(data.Key))
 
   err := binary.Write(f, binary.LittleEndian, bytes)
   if err != nil {
     log.Fatal(err)
+    return bcount, err
   }
-  numBytes, _ := f.WriteString(data.Key)
-  log.Printf("bytes = %d, numBytes = %d", bytes, numBytes)
+  bcount += 4
+
+  numBytes, err := f.WriteString(data.Key)
+  if err != nil {
+    log.Fatal(err)
+    return bcount, err
+  }
+  bcount += numBytes
+  //log.Printf("bytes = %d, numBytes = %d", bytes, numBytes)
+
+  bytes = int32(len(data.Value))
+  err = binary.Write(f, binary.LittleEndian, bytes)
+  if err != nil {
+    log.Fatal(err)
+    return bcount, err
+  }
+  bcount += 4
+
+
+  numBytes, err = f.Write(data.Value)
+  if err != nil {
+    log.Fatal(err)
+    return bcount, err
+  }
+  bcount += numBytes
+  //log.Printf("bytes = %d, numBytes = %d", bytes, numBytes)
+
+  err = binary.Write(f, binary.LittleEndian, data.Deleted)
+  if err != nil {
+    log.Fatal(err)
+    return bcount, err
+  }
+  bcount += 1
+
+  return bcount, nil
 }
--- lsm/sst/binary_test.go
+++ lsm/sst/binary_test.go
@@ -13,5 +13,12 @@ func TestBinary(t *testing.T) {
   }
   defer f.Close()
 
-  binaryWrite(f, SstEntry{"my key", []byte("my data"), false})
+  var lis = [] SstEntry {
+    SstEntry{"my key 1", []byte("my data 1"), true},
+    SstEntry{"my key 2", []byte("my data 2"), false},
+    SstEntry{"my key 3", []byte("my data 3"), true},
+    SstEntry{"my key 4", []byte("my data 4"), false},
+    SstEntry{"my key 5", []byte("my data 5"), false} }
+
+  writeEntries(f, lis)
 }
