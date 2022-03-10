34303bc5086a591ff761a836cae211e46460fb15
Justin Ethier <justin.ethier@gmail.com>
2022-03-09 19:29:31 -0800

    Read entries

--- lsm/sst/binary-io.go
+++ lsm/sst/binary-io.go
@@ -3,31 +3,57 @@ package sst
 import (
 	"encoding/binary"
 	"unicode/utf8"
+	"io"
 	"log"
 	"os"
 )
 
 func readEntries(f *os.File) ([]SstEntry, error) {
-  readEntry(f)
+  for {
+    _, err := readEntry(f)
+    if err != nil {
+      break
+    }
+  }
   return nil, nil
 }
 
-func readEntry(f *os.File) ([]SstEntry, error){
+func readEntry(f *os.File) (SstEntry, error){
   var length int32
+  var e SstEntry
 
   err := binary.Read(f, binary.LittleEndian, &length)
   if err != nil {
     log.Fatal(err)
-    return nil, err
+    return e, err
   }
   log.Println("key length", length)
 
-  // TODO: use f.ReadFull to read key
-  // TODO: read value length
-  // TODO: use f.ReadFull to read value
-  // TODO: read deleted
+  var keybuf = make([]byte, int(length))
+  _, err = io.ReadFull(f, keybuf)
+  e.Key = string(keybuf)
 
-  return nil, nil
+  // TODO: read value length
+  err = binary.Read(f, binary.LittleEndian, &length)
+  if err != nil {
+    log.Fatal(err)
+    return e, err
+  }
+  log.Println("value length", length)
+
+  // TODO: use f.ReadFull to read value
+  var valbuf = make([]byte, int(length))
+  _, err = io.ReadFull(f, valbuf)
+  e.Value = valbuf
+
+  err = binary.Read(f, binary.LittleEndian, &e.Deleted)
+  if err != nil {
+    log.Fatal(err)
+    return e, err
+  }
+  log.Println("entry", e)
+
+  return e, nil
 }
 
 func writeEntries(f *os.File, entries []SstEntry) (int, error){
