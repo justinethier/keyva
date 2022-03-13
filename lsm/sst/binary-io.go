27bf054838c175dd0b6cd946be34d3e705598eb8
Justin Ethier <justin.ethier@gmail.com>
2022-03-13 08:06:10 -0700

    Finish readIndex, cleanup

--- lsm/sst/binary-io.go
+++ lsm/sst/binary-io.go
@@ -2,179 +2,220 @@ package sst
 
 import (
 	"encoding/binary"
-	"unicode/utf8"
 	"io"
 	"log"
 	"os"
+	"unicode/utf8"
 )
 
+// readEntries reads all entries from the given SST file pointer and
+// returns them as an array
 func readEntries(f *os.File) []SstEntry {
-  var lis []SstEntry
-  var err error
-  var e SstEntry
+	var lis []SstEntry
+	var err error
+	var e SstEntry
 
-  for err == nil {
-    e, err = readEntry(f)
-    if err == nil {
-      lis = append(lis, e)
-    }
-  }
-  return lis
+	for err == nil {
+		e, err = readEntry(f)
+		if err == nil {
+			lis = append(lis, e)
+		}
+	}
+	return lis
 }
 
-func readEntry(f *os.File) (SstEntry, error){
-  var length int32
-  var e SstEntry
+// readEntry reads a single entry from the given SST file pointer
+func readEntry(f *os.File) (SstEntry, error) {
+	var length int32
+	var e SstEntry
 
-  err := binary.Read(f, binary.LittleEndian, &length)
-  if err != nil {
-    if err != io.EOF {
-      log.Fatal(err)
-    }
-    return e, err
-  }
-  //log.Println("key length", length)
+	err := binary.Read(f, binary.LittleEndian, &length)
+	if err != nil {
+		if err != io.EOF {
+			log.Fatal(err)
+		}
+		return e, err
+	}
+	//log.Println("key length", length)
 
-  var keybuf = make([]byte, int(length))
-  _, err = io.ReadFull(f, keybuf)
-  e.Key = string(keybuf)
+	var keybuf = make([]byte, int(length))
+	_, err = io.ReadFull(f, keybuf)
+	e.Key = string(keybuf)
 
-  // TODO: read value length
-  err = binary.Read(f, binary.LittleEndian, &length)
-  if err != nil {
-    log.Fatal(err)
-    return e, err
-  }
-  //log.Println("value length", length)
+	// TODO: read value length
+	err = binary.Read(f, binary.LittleEndian, &length)
+	if err != nil {
+		log.Fatal(err)
+		return e, err
+	}
+	//log.Println("value length", length)
 
-  // TODO: use f.ReadFull to read value
-  var valbuf = make([]byte, int(length))
-  _, err = io.ReadFull(f, valbuf)
-  e.Value = valbuf
+	// TODO: use f.ReadFull to read value
+	var valbuf = make([]byte, int(length))
+	_, err = io.ReadFull(f, valbuf)
+	e.Value = valbuf
 
-  err = binary.Read(f, binary.LittleEndian, &e.Deleted)
-  if err != nil && err != io.EOF {
-    log.Fatal(err)
-    return e, err
-  }
-  //log.Println("entry", e)
-  return e, nil
+	err = binary.Read(f, binary.LittleEndian, &e.Deleted)
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+		return e, err
+	}
+	//log.Println("entry", e)
+	return e, nil
 }
 
-func writeSst(filename string, keys []string, m map[string]SstEntry, seqNum uint64) {
-  f, err := os.Create(filename + ".bin")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer f.Close()
+// writeSst creates an SST file and corresponding index file using the given data.
+// seqNum is the sequence number of the latest entry.
+// keysPerSegment is the number of keys that will be stored for each sparse index.
+func writeSst(filename string, keys []string, m map[string]SstEntry, seqNum uint64, keysPerSegment int) {
+	f, err := os.Create(filename + ".bin")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-  findex, err := os.Create(filename + ".index")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer findex.Close()
+	findex, err := os.Create(filename + ".index")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer findex.Close()
 
-  // write seq header to index file
-  err = binary.Write(findex, binary.LittleEndian, seqNum)
+	// write seq header to index file
+	err = binary.Write(findex, binary.LittleEndian, seqNum)
 
-  // TODO: write every nth entry tp index file (sparse index)
-  var offset int = 0
+	// TODO: write every nth entry tp index file (sparse index)
+	var offset int = 0
 	for i, k := range keys {
 		var e SstEntry
 		e = m[k]
 		bytes, err := writeEntry(f, e)
 		if err != nil {
-      log.Fatal(err)
-    }
-    if (i % 2) == 0 { // TODO: larger and configurable interval
-      writeIndex(findex, e, bytes)
-    }
-    offset += bytes
+			log.Fatal(err)
+		}
+		if (i % keysPerSegment) == 0 {
+			writeKeyToIndex(findex, e.Key, bytes)
+		}
+		offset += bytes
 	}
 }
 
+// readIndex reads and returns the contents of the given SST index file pointer.
 func readIndex(f *os.File) ([]SstIndex, SstFileHeader, error) {
-  var header SstFileHeader
-  var index []SstIndex
-  err := binary.Read(f, binary.LittleEndian, &header.Seq)
-  if err != nil {
-    log.Fatal(err)
-    return index, header, err
-  }
+	var header SstFileHeader
+	var index []SstIndex
+	err := binary.Read(f, binary.LittleEndian, &header.Seq)
+	if err != nil {
+		log.Fatal(err)
+		return index, header, err
+	}
 
-  for contents of index
-    read key length
-    read key
-    read offset
-    package into SstIndex, append to list
+	var length int32
+	for err == nil {
+		var entry SstIndex
+		// read key length
+		err = binary.Read(f, binary.LittleEndian, &length)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal(err)
+			}
+		}
+		// read key
+		var keybuf = make([]byte, int(length))
+		_, err = io.ReadFull(f, keybuf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal(err)
+			}
+		}
+		entry.Key = string(keybuf)
 
-  return index, header, nil
+		//read offset
+		err = binary.Read(f, binary.LittleEndian, &length)
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			}
+		}
+		entry.offset = int(length)
+
+		index = append(index, entry)
+	}
+
+	return index, header, nil
 }
 
-func writeIndex(f *os.File, data SstEntry, offset int) error {
-  // key length
-  var bytes int32 = int32(utf8.RuneCountInString(data.Key))
-  err := binary.Write(f, binary.LittleEndian, bytes)
-  if err != nil {
-    log.Fatal(err)
-    return err
-  }
-  // key
-  _, err = f.WriteString(data.Key)
-  if err != nil {
-    log.Fatal(err)
-    return err
-  }
-  // offset
-  err = binary.Write(f, binary.LittleEndian, int32(offset))
-  if err != nil {
-    log.Fatal(err)
-    return err
-  }
-  return nil
+// writeKeyToIndex writes a sparse index to the given SST index file pointer.
+// key is the SST key that is being written as a sparse index.
+// offset is the byte offset of the key in the corresponding SST file.
+func writeKeyToIndex(f *os.File, key string, offset int) error {
+	// key length
+	var bytes int32 = int32(utf8.RuneCountInString(key))
+	err := binary.Write(f, binary.LittleEndian, bytes)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	// key
+	_, err = f.WriteString(key)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	// offset
+	err = binary.Write(f, binary.LittleEndian, int32(offset))
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
 }
 
-func writeEntry(f *os.File, data SstEntry) (int, error){
-  var bcount int = 0
-  var bytes int32 = int32(utf8.RuneCountInString(data.Key))
+// writeEntry writes data for a single key/value pair to file
+func writeEntry(f *os.File, data SstEntry) (int, error) {
+	var bcount int = 0
+	var bytes int32 = int32(utf8.RuneCountInString(data.Key))
 
-  err := binary.Write(f, binary.LittleEndian, bytes)
-  if err != nil {
-    log.Fatal(err)
-    return bcount, err
-  }
-  bcount += 4
+	err := binary.Write(f, binary.LittleEndian, bytes)
+	if err != nil {
+		log.Fatal(err)
+		return bcount, err
+	}
+	bcount += 4
 
-  numBytes, err := f.WriteString(data.Key)
-  if err != nil {
-    log.Fatal(err)
-    return bcount, err
-  }
-  bcount += numBytes
-  //log.Printf("bytes = %d, numBytes = %d", bytes, numBytes)
+	numBytes, err := f.WriteString(data.Key)
+	if err != nil {
+		log.Fatal(err)
+		return bcount, err
+	}
+	bcount += numBytes
+	//log.Printf("bytes = %d, numBytes = %d", bytes, numBytes)
 
-  bytes = int32(len(data.Value))
-  err = binary.Write(f, binary.LittleEndian, bytes)
-  if err != nil {
-    log.Fatal(err)
-    return bcount, err
-  }
-  bcount += 4
+	bytes = int32(len(data.Value))
+	err = binary.Write(f, binary.LittleEndian, bytes)
+	if err != nil {
+		log.Fatal(err)
+		return bcount, err
+	}
+	bcount += 4
 
-  numBytes, err = f.Write(data.Value)
-  if err != nil {
-    log.Fatal(err)
-    return bcount, err
-  }
-  bcount += numBytes
-  //log.Printf("bytes = %d, numBytes = %d", bytes, numBytes)
+	numBytes, err = f.Write(data.Value)
+	if err != nil {
+		log.Fatal(err)
+		return bcount, err
+	}
+	bcount += numBytes
+	//log.Printf("bytes = %d, numBytes = %d", bytes, numBytes)
 
-  err = binary.Write(f, binary.LittleEndian, data.Deleted)
-  if err != nil {
-    log.Fatal(err)
-    return bcount, err
-  }
-  bcount += 1
+	err = binary.Write(f, binary.LittleEndian, data.Deleted)
+	if err != nil {
+		log.Fatal(err)
+		return bcount, err
+	}
+	bcount += 1
 
-  return bcount, nil
+	return bcount, nil
 }
--- lsm/sst/binary_test.go
+++ lsm/sst/binary_test.go
@@ -9,50 +9,61 @@ import (
 )
 
 func TestBinary(t *testing.T) {
-  var keys []string
+	var keys []string
 	m := make(map[string]SstEntry)
 
-  for i := 0; i < 10; i++ {
-    key := strconv.Itoa(i)
-    keys = append(keys, key)
-    m[key] = SstEntry{key, []byte("Test Value"), false}
-  }
+	for i := 0; i < 10; i++ {
+		key := strconv.Itoa(i)
+		keys = append(keys, key)
+		m[key] = SstEntry{key, []byte("Test Value"), false}
+	}
 
-  writeSst("mytest", keys, m, uint64(10))
+	writeSst("mytest", keys, m, uint64(10), 3)
 }
 
 func TestBinaryRead(t *testing.T) {
-  f, err := os.Open("mytest.bin")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer f.Close()
+	f, err := os.Open("mytest.bin")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-  findex, err := os.Open("mytest.index")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer findex.Close()
+	findex, err := os.Open("mytest.index")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer findex.Close()
 
-  // TODO: validate index file contents
-  _, header, err := readIndex(findex)
-  if header.Seq != uint64(10) {
-    t.Error("Unexpected sequence number", header.Seq)
-  }
+	// validate index file contents
+	index, header, err := readIndex(findex)
+	if len(index) != 4 {
+		t.Error("Expected index of length 4 but received one of length", len(index))
+	}
+	if header.Seq != uint64(10) {
+		t.Error("Unexpected sequence number", header.Seq)
+	}
 
-  // Validate contents of SST
-  lis := readEntries(f)
-  log.Println("read entries", len(lis))
-  for i, e := range lis {
-    key := strconv.Itoa(i)
-    if key != e.Key {
-      t.Error("Expected key", key, "but received", e.Key)
-    }
-    if bytes.Compare(e.Value, []byte("Test Value")) != 0 {
-      t.Error("Unexpected data", e.Value)
-    }
-    if e.Deleted != false {
-      t.Error("Unexpected deleted flag", e.Deleted)
-    }
-  }
+	// Validate contents of index
+	for i, e := range index {
+		key := strconv.Itoa(i * 3)
+		if key != e.Key {
+			t.Error("Expected index key", key, "but received", e.Key)
+		}
+	}
+
+	// Validate contents of SST
+	lis := readEntries(f)
+	log.Println("read entries", len(lis))
+	for i, e := range lis {
+		key := strconv.Itoa(i)
+		if key != e.Key {
+			t.Error("Expected key", key, "but received", e.Key)
+		}
+		if bytes.Compare(e.Value, []byte("Test Value")) != 0 {
+			t.Error("Unexpected data", e.Value)
+		}
+		if e.Deleted != false {
+			t.Error("Unexpected deleted flag", e.Deleted)
+		}
+	}
 }
