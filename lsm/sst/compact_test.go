736e178c6c8ce9e24478d2253a5defbe88828f89
Justin Ethier <justin.ethier@gmail.com>
2022-03-28 10:43:49 -0700

    Use new binary test files

--- lsm/sst/compact_test.go
+++ lsm/sst/compact_test.go
@@ -10,9 +10,9 @@ func TestSstCompact(t *testing.T) {
 
 	files = append(files, "./test-data/sst-0000.bin")
 	files = append(files, "./test-data/sst-0001.bin")
-	files = append(files, "./test-data/sst-0002.bin")
+	//files = append(files, "./test-data/sst-0002.bin")
 	newdir, _ := Compact(files, "test-data", 100, 10, false)
-	if !util.DeepCompare(newdir+"/sst-0000.bin", "test-data/compacted.sst") {
+	if !util.DeepCompare(newdir+"/sst-0000.bin", "test-data/compacted.bin") {
 		t.Error("Compacted SST file does not contain expected contents", "newsst")
 	}
 }
