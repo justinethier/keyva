686b9dfcfebac5378b5d9e45f795bbfb81472a0d
Justin Ethier <justin.ethier@gmail.com>
2022-04-05 18:03:04 -0700

    Fix failing tests

--- lsm/sst/find_test.go
+++ lsm/sst/find_test.go
@@ -11,8 +11,8 @@ func TestFindIndex(t *testing.T) {
 	u := SstIndex{"uu", 300}
 	index := []SstIndex{e, j, m, u}
 
-	check := func(key string, start int, end int, found bool) {
-		iKey, iNext, f := findIndex(key, index)
+	check := func(key string, start int, end int, idx int, found bool) {
+		iKey, iNext, i, f := findIndex(key, index)
 		var s, e int
 
    if iKey != nil {
@@ -26,6 +26,10 @@ func TestFindIndex(t *testing.T) {
 			t.Error("Key", key, "Expected found", found, "but received", f)
 			return
 		}
+		if i != idx {
+			t.Error("Key", key, "Expected index", idx, "but received", i)
+			return
+		}
 		if s != start {
 			t.Error("Key", key, "Expected starting offset", start, "but received", s)
 		}
@@ -33,17 +37,17 @@ func TestFindIndex(t *testing.T) {
 			t.Error("Key", key, "Expected ending offset", end, "but received", e)
 		}
 	}
-	check("aa", 0, 0, false)
-	check("b", 0, 0, false)
-	check("ccc", 0, 0, false)
-	check("dc", 0, 0, false)
-	check("ee", 0, 100, true)
-	check("ff", 0, 100, true)
-	check("gg", 0, 100, true)
-	check("jj", 100, 200, true)
-	check("ll", 100, 200, true)
-	check("qq", 200, 300, true)
-	check("ss", 200, 300, true)
-	check("xx", 300, 0, true)
-	check("zz", 300, 0, true)
+	check("aa", 0, 0, -1, false)
+	check("b", 0, 0, -1, false)
+	check("ccc", 0, 0, -1, false)
+	check("dc", 0, 0, -1, false)
+	check("ee", 0, 100, 0, true)
+	check("ff", 0, 100, 0, true)
+	check("gg", 0, 100, 0, true)
+	check("jj", 100, 200, 1, true)
+	check("ll", 100, 200, 1, true)
+	check("qq", 200, 300, 2, true)
+	check("ss", 200, 300, 2, true)
+	check("xx", 300, 0, 3, true)
+	check("zz", 300, 0, 3, true)
 }
